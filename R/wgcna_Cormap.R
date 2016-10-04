38a338a36b3fabd0b825bbf8d50aeb0516762217
root <anthonycolombo60@gmail.com>
2016-10-04 15:28:22 -0700

    reordering correctly.

--- R/wgcna_Cormap.R
+++ R/wgcna_Cormap.R
@@ -24,62 +24,8 @@ moduleTraitPvalue<-lnames[["moduleTraitPvalue"]]
 # Will display correlations and their p-values
 # Define numbers of genes and samples
 nGenes= ncol(datExpr);
-nSamples = nrow(datExpr);
-# Recalculate MEs with color labels
-MEs0 = moduleEigengenes(datExpr, bwModuleColors)$eigengenes
-MEs = orderMEs(MEs0)
-if(recalc==TRUE){
-moduleTraitCor = bicor(MEs, datTraits, use = "all.obs");
-moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nSamples);
-} else{
-moduleTraitCor<-lnames[["moduleTraitCor"]]
-moduleTraitPvale<-lnames[["moduleTraitPvalue"]]
-}
-
-
-textMatrix =  paste(signif(moduleTraitCor, 2), "\n(",
-                           signif(moduleTraitPvalue, 1), ")", sep = "");
-
-dim(textMatrix) = dim(moduleTraitCor)
-moduleTraitCor<-moduleTraitCor[order(moduleTraitCor[,1],decreasing=TRUE),] 
-par(mar = c(6, 10, 3, 3));
-# Display the correlation values within a heatmap plot
-plot.new()
-labeledHeatmap(Matrix = moduleTraitCor,
-               xLabels = names(datTraits),
-               yLabels = names(MEs),
-               ySymbols = names(MEs),
-               colorLabels = FALSE,
-               colors = greenWhiteRed(50),
-               textMatrix = textMatrix,
-               setStdMargins = FALSE,
-               cex.text = 0.3,
-               zlim = c(-1,1),
-               yColorWidth=0.07,
-               cex.lab.y=.7,
-               colors.lab.y=1.3,
-               main = paste0("Module-Repeat Biotype TMM cut:",read.cutoff))
-
-
-readkey()
-colorDF<-sapply(MEs,function(x) median(x))
-colorDF<-colorDF[order(colorDF,decreasing=TRUE)]
-  if(plotDot==TRUE){
-  plot.new()
-  par(mar = c(11.5, 6, 3, 3));
-  plot(colorDF,main="Median Correlation Per Module")
-  axis(1,at=1:length(colorDF),labels=names(colorDF),las=2)
-  readkey()
-  } else {
-  pdf("correlation_plots.pdf")
-    par(mar = c(11.5, 6, 3, 3));
-    labeledHeatmap(Matrix = moduleTraitCor,
-               xLabels = names(datTraits),
-               yLabels = names(MEs),
-               ySymbols = names(MEs),
-               colorLabels = FALSE,
-               colors = greenWhiteRed(50),
-               textMatrix = textMatrix,
+tmes<-rownames(moduleTraitCor[order(moduleTraitCor[,1],decreasing=TRUE),])
+extMatrix,
                setStdMargins = FALSE,
                cex.text = 0.3,
                zlim = c(-1,1),
