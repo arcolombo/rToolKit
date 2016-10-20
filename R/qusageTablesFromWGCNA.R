3d67144d25dd05ef0eb42d56b462e2c2a1b748d2
root <anthonycolombo60@gmail.com>
2016-10-20 11:23:17 -0700

    fixed a patch with qusageTables. qusageTables collapsed by ENSGID, then queries the module, and collapses module by gene name, normalizes and log2XRs.  this is an update

--- R/qusageTablesFromWGCNA.R
+++ R/qusageTablesFromWGCNA.R
@@ -13,9 +13,9 @@
 #' @return a qusageDbLite db
 qusageTablesFromWGCNA<-function(kexp,verbose=TRUE,dbname="wgcnaDBLite.sqlite",version="1.0.0",Module.color="brown",MsigDB=c("c1.all.v5.1.symbols.gmt","c2.all.v5.1.symbols.gmt","c4.all.v5.1.symbols.gmt","c5.all.v5.1.symbols.gmt","c6.all.v5.1.symbols.gmt","c7.all.v5.1.symbols.gmt","h.all.v5.1.symbols.gmt"),how=c("cpm","tpm"),species=c("Homo.sapiens","Mus.musculus") ){
 
-##task: this will take a full kexp and collapse by gene_name then  tmm normalize and log2 transform or tpm normalize and pass into qusageRun.R to handle the pre-proccessing for qusage call.  the output should be module.biotype.enrich specific data.  then write to a db. 
+##task: this will take a full kexp and first normalize then collapse by gene_name then log2 transform or tpm normalize and pass into qusageRun.R to handle the pre-proccessing for qusage call.  the output should be module.biotype.enrich specific data.  then write to a db. 
  ##important note: it is tempting to merely use collapseBundles(kexp,"gene_name") and pipe into qusage HOWEVER QUSAGE REQUIRES NORMALIZED LOG2 XR. collapseBundles will only use the raw counts(kexp) bundle gene_name counts.  here we *****MUST******* use tmm normalized or Tpm calls.
-##ALSO NOTE: you can first normalize and then collapse by gene name, but that is not that same as collapsing by gene name and then normalizing.  we see better results for the latter.  
+##ALSO NOTE: you can first collapse by gene_id normalize w.r.t gene_id and then collapse by gene name, but that is not that same as collapsing by gene_id, querying a module, then collapsing by gene name and then normalizing by gene_name.  we see better results for the latter
 
 
    tnxomes<-transcriptomes(kexp)
@@ -25,10 +25,10 @@ qusageTablesFromWGCNA<-function(kexp,verbose=TRUE,dbname="wgcnaDBLite.sqlite",ve
    how<-match.arg(how,c("cpm","tpm"))
    geneSet<-match.arg(MsigDB,c("c1.all.v5.1.symbols.gmt","c2.all.v5.1.symbols.gmt","c4.all.v5.1.symbols.gmt","c5.all.v5.1.symbols.gmt","c6.all.v5.1.symbols.gmt","c7.all.v5.1.symbols.gmt","h.all.v5.1.symbols.gmt"))
 
-##### the modules are in terms of ENSG IDs and we are going to build a qusage database of the modules, so we must collapse counts by ENSG IDs,normalize, log2XR and then annotate to get hgnc to pipe into qusage.  
+##### the modules are in terms of ENSG IDs and we are going to build a qusage database of the modules, so we must collapse counts by ENSG IDs, subset the module, collapse module genes by gene_name, and then normalize, log2XR and then annotate to get hgnc to pipe into qusage.  
 #####
   
-  ##Lazy Species detection
+ 
     if(how=="cpm"){
   ##TMM normalize
   full_counts<-collapseBundles(kexp,"gene_id")
@@ -68,11 +68,15 @@ qusageTablesFromWGCNA<-function(kexp,verbose=TRUE,dbname="wgcnaDBLite.sqlite",ve
    wgcna.color<-modulesBy(wgcnaDbLite(dbname),p.value=2,Module.color=allcolors[i])
    color.id<-match(wgcna.color$row_names,rownames(full_counts))
    stopifnot(all(rownames(full_counts[color.id,] )==wgcna.color$row_names)) ##check
-module.counts<-full_counts[color.id,]
-  ##we found better results for first collapse by gene name and then normalizing.  as opposed to normalizing first and then mapping to gene names.
+   ##subset module by ENSGID
+  module.counts<-full_counts[color.id,]
+  module.counts<-module.counts[,which(colSums(module.counts)>1)]
+  ## first collapse by gene name and then normalize.  as opposed to normalizing gene_ids and then collapsing the modules.
+   ##module kexp
   module.kexp<-kexp[rowRanges(kexp)$gene_id%in%rownames(module.counts),]
    if(how=="cpm"){
    counts<-collapseBundles(module.kexp,"gene_name")
+   counts<-counts[,which(colSums(counts)>1)]
    dge<-DGEList(counts=counts)
    dge<-calcNormFactors(dge)
    expr<-cpm(dge,log=FALSE)
@@ -92,20 +96,41 @@ module.counts<-full_counts[color.id,]
     ##call qusage for each stage
     qusage_run1<-qusageRun(cnts_mt=cnts_phLS,MsigDB=MsigDB,comparison="pHSC",control="LSC",module=allcolors[i])
     qusage_run2<-qusageRun(cnts_mt=cnts_blsLS,MsigDB=MsigDB,comparison="Blast",control="LSC",module=allcolors[i])
+ 
+      if(nrow(qusage_run1[!is.na(qusage_run1$p.Value),])>1){
       qusage_run1<-data.frame(qusage_run1[!is.na(qusage_run1$p.Value),],
                              colorKey=allcolors[i],
                              bioKey=allcolors[i],
                              contrastKey="phsc")
-                                
-       qusage_run2<-data.frame(qusage_run2[!is.na(qusage_run2$p.Value),] ,
+       }else{
+         qusage_run1<-data.frame(pathway.name="NA",
+                                    log.fold.change="NA",
+                                    p.Value="NA",
+                                    FDR="NA",
+                                    colorKey=allcolors[i],
+                                    bioKey=allcolors[i],
+                                    contrastKey="phsc")
+       } 
+      if(nrow(qusage_run2[!is.na(qusage_run2$p.Value),])>1){                             qusage_run2<-data.frame(qusage_run2[!is.na(qusage_run2$p.Value),] ,
                               colorKey=allcolors[i], 
                               bioKey=allcolors[i],
                               contrastKey="blast")
+
+       }else{
+         qusage_run2<-data.frame(pathway.name="NA",
+                                    log.fold.change="NA",
+                                    p.Value="NA",
+                                    FDR="NA",
+                                    colorKey=allcolors[i],
+                                    bioKey=allcolors[i],
+                                    contrastKey="blast")
+
+       }
    ##entire module universe of enrichment.
           qusage_module<-rbind(qusage_run1,qusage_run2) 
     for( j in 1:length(allTraits)){
    ########SECOND FOR LOOP ACROSS ALL TRAITS HERE #########
-   ###here we filter the most significant correlated module ~ trait driver and run enrichment on these
+   ###here we filter the most significant correlated module ~ trait (driver) associations and run enrichment on these
   if(verbose) cat("Extracting Trait Driver Enrichments...\n")  
   ##grabs the wgcnaDbLite db 
  wgcna.color.trait<-traitsBy(wgcnaDbLite(dbname),p.value=0.05,Module.color=allcolors[i],trait=allTraits[j]) 
@@ -123,10 +148,11 @@ module.counts<-full_counts[color.id,]
    trait.color.id<-match(wgcna.color.trait$row_names,rownames(full_counts))
    stopifnot(rownames(full_counts[trait.color.id,])==wgcna.color.trait$row_names)
    module.trait.counts<-full_counts[trait.color.id,]
-    ##we found better results for first collapse by gene name and then normalizing.  as opposed to normalizing first and then mapping to gene names.
+    ## collapse by gene name and then normalize moduleKexp.  as opposed to normalizing first and then mapping to gene names.
   module.trait.kexp<-kexp[rowRanges(kexp)$gene_id%in%rownames(module.trait.counts),]
    if(how=="cpm"){
    module.trait.counts<-collapseBundles(module.trait.kexp,"gene_name")
+   module.trait.counts<-module.trait.counts[,which(colSums(module.trait.counts)>1)]
    if(nrow(module.trait.counts)<=8){
    next
    }
@@ -152,15 +178,39 @@ module.counts<-full_counts[color.id,]
     ##call qusage for each stage
     qusage_trait.run1<-qusageRun(cnts_mt=trait.cnts_phLS,MsigDB=MsigDB,comparison="pHSC",control="LSC",module=allcolors[i])
     qusage_trait.run2<-qusageRun(cnts_mt=trait.cnts_blsLS,MsigDB=MsigDB,comparison="Blast",control="LSC",module=allcolors[i])
-      qusage_trait.run1<-data.frame(qusage_trait.run1,
+     
+     if(nrow(qusage_trait.run1[!is.na(qusage_trait.run1$p.Value),])>1){
+     qusage_trait.run1<-data.frame(qusage_trait.run1[!is.na(qusage_trait.run1$p.Value),],
                              colorKey=allcolors[i],
                              bioKey=allTraits[j],
                              contrastKey="phsc")
+     }else{
+        qusage_trait.run1<-data.frame(pathway.name="NA",
+                                    log.fold.change="NA",
+                                    p.Value="NA",
+                                    FDR="NA",
+                                    colorKey=allcolors[i],
+                                    bioKey=allTraits[j],
+                                    contrastKey="phsc")
 
-       qusage_trait.run2<-data.frame(qusage_trait.run2,
+     }
+     if(nrow(qusage_trait.run2[!is.na(qusage_trait.run2$p.Value),])>1){
+       qusage_trait.run2<-data.frame(qusage_trait.run2[!is.na(qusage_trait.run2$p.Value),],
                               colorKey=allcolors[i],
                               bioKey=allTraits[j],
                               contrastKey="blast")
+
+     }else{
+     qusage_trait.run2<-data.frame(pathway.name="NA",
+                                    log.fold.change="NA",
+                                    p.Value="NA",
+                                    FDR="NA",
+                                    colorKey=allcolors[i],
+                                    bioKey=allTraits[j],
+                                    contrastKey="blast")
+
+       }
+   
       qusage_module.trait<-rbind(qusage_trait.run1,qusage_trait.run2)
   
       qusage_module<-rbind(qusage_module,qusage_module.trait)
@@ -174,7 +224,7 @@ module.counts<-full_counts[color.id,]
  }## END ALL i allcolors loop
   if(verbose) cat("Writing enrichment of entire kexp...\n")
   
- ##FIX ME: call for the whole pHSC-LSC and Blast-LSC as separate tables
+ ##FIX ME: call for the ***whole*** pHSC-LSC and Blast-LSC as separate tables
  ## take the database and query the geneIDs of interest, and use the kexp on those geneIDs
   if(how=="cpm"){
   ##TMM normalize
