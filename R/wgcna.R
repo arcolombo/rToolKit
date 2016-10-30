8747fe2e03fca6e50e3aad336ecf3d33f421c1df
root <anthonycolombo60@gmail.com>
2016-10-30 08:43:27 -0700

    adding power plot analyiss to pdf.

--- R/wgcna.R
+++ R/wgcna.R
@@ -13,7 +13,7 @@
 #' @import edgeR
 #' @export
 #' @return images and cluster at the gene and repeat level
-wgcna<-function(kexp,read.cutoff=2,minBranch=2,whichWGCNA=c("single","block"),entrezOnly=FALSE,species=c("Homo.sapiens","Mus.musculus"),selectedPower=NULL,intBiotypes=c("Alu","DNA transposon","Endogenous Retrovirus","ERV1","ERV3","ERVK","ERVL","L1","L2","LTR Retrotransposon","Satellite"),useAllBiotypes=FALSE,tmm.norm=TRUE,useBiCor=TRUE,how=c("cpm","tpm"),bathNormalize=FALSE,batchVector=NULL){
+wgcna<-function(kexp,read.cutoff=2,minBranch=2,whichWGCNA=c("single","block"),entrezOnly=FALSE,species=c("Homo.sapiens","Mus.musculus"),selectedPower=NULL,intBiotypes=c("Alu","DNA transposon","Endogenous Retrovirus","ERV1","ERV3","ERVK","ERVL","L1","L2","LTR Retrotransposon","Satellite"),useAllBiotypes=FALSE,tmm.norm=TRUE,useBiCor=TRUE,how=c("cpm","tpm"),batchNormalize=FALSE,batchVector=NULL){
   ##FIX ME: add option for TPMs as well as  CPM. Add TPM support
    how<-match.arg(how,c("cpm","tpm"))
   ##prepare data
@@ -163,6 +163,26 @@ if (!gsg$allOK)
    text(sft$fitIndices[,1], sft$fitIndices[,5],
      labels=powers,cex=cex1,col="red");
   selectedPower<-readPower()
+  ####Print to PDF############
+  pdf(paste0("selectedPower_",how,"analysis.pdf"))
+  sizeGrWindow(9, 5)
+  par(mfrow = c(1,2));
+  cex1 = 0.9;
+  # Scale-free topology fit index as a function of the soft-thresholding power
+  plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
+     main = paste("Scale independence"));
+  text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     labels=powers,cex=cex1,col="red");
+  # this line corresponds to using an R^2 cut-off of h
+  abline(h=0.90,col="red")
+   plot(sft$fitIndices[,1], sft$fitIndices[,5],
+     xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
+     main = paste("Mean connectivity"))
+   text(sft$fitIndices[,1], sft$fitIndices[,5],
+     labels=powers,cex=cex1,col="red");
+  dev.off()
+
   } #selectedPower NULL
   message("annotating...")
   datExpr<-as.data.frame(datExpr,stringsAsFactors=FALSE)
