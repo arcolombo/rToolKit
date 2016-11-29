b157f779136bf78f9ea3c5f5ef5c881703dd6aec
root <anthonycolombo60@gmail.com>
2016-11-29 08:29:54 -0800

    connectivity analysis will go through the kME which is the connectivity of the module Eigenvalues ~ genes to find the hightest hubs to ME.   leaves out some details but uses signedKME function

--- R/connectivityWiseAnalysis.R
+++ R/connectivityWiseAnalysis.R
@@ -2,7 +2,7 @@
 #' @description tests the significance of connectivity of genes in a given pathway resulting in significance tests of connected genes of pathway.
 #' @import WGCNA
 
-connectivityWiseAnalysis<-function(lnames,MsigDB=c("c1.all.v5.1.symbols.gmt","c2.all.v5.1.symbols.gmt","c4.all.v5.1.symbols.gmt","c5.all.v5.1.symbols.gmt","c6.all.v5.1.symbols.gmt","c7.all.v5.1.symbols.gmt","h.all.v5.1.symbols.gmt"), pathway=, dbname="wgcnaDBLite.sqlite",qusageDbName="qusageDbLite",gmt.path="~/Documents/Arkas-Paper-Data/MSigDB/MsigDb_all/",moduleColor="violet" ){
+connectivityWiseAnalysis<-function(lnames,MsigDB=c("c1.all.v5.1.symbols.gmt","c2.all.v5.1.symbols.gmt","c4.all.v5.1.symbols.gmt","c5.all.v5.1.symbols.gmt","c6.all.v5.1.symbols.gmt","c7.all.v5.1.symbols.gmt","h.all.v5.1.symbols.gmt"), pathway=NULL, dbname="wgcnaDBLite.sqlite",qusageDbName="qusageDbLite",gmt.path="~/Documents/Arkas-Paper-Data/MSigDB/MsigDb_all/",moduleColor="violet" ){
 
  bwModuleColors<-lnames[["moduleColors"]]
   MEs<-lnames[["MEs"]]
@@ -26,6 +26,7 @@ geneModuleMembership1<-signedKME(datExpr,MEs)
 
 ###need to collapse kME by gene name?
 
+for(i in c(1,2,3,4,5,6,8,9,10,12,14,16,17,21,23,24,25,27,31,32,33,39)){+ write.csv(sort(unique(feats[feats$gene_id%in%rownames(kme1)[which(abs(kme1[,i])>=0.80)]]$gene_name)),file=paste0(rownames(moduleTraitCor)[i],"_",i,"_topKME_.80.csv"))}
 
 
 
