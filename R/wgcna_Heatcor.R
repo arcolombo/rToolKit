1626ef583a625e4fa59fe02989000a92275cd302
root <anthonycolombo60@gmail.com>
2016-12-08 14:50:46 -0800

    wgcna_Heatcor now prints out picked pathways to csv. stars significant

--- R/wgcna_Heatcor.R
+++ R/wgcna_Heatcor.R
@@ -10,6 +10,7 @@
 #' @import ComplexHeatmap
 #' @import pvclust
 #' @import dendsort
+#' @import plyr
 #' @export
 #' @return images of eigengenes
 wgcna_Heatcor<-function(lnames=NULL,rnames=NULL, read.cutoff=2,recalc=FALSE,how=how,pathwaysToPick=c("immune","inflam","apopto","death","kappab","wound"),pathPairing=c(1,1,2,2,3,4),dbname=NULL,qdbname=NULL){
@@ -51,14 +52,16 @@ message(paste0("found lnames"))
  # if(orderBiotype=="L1"){
  #moduleTraitCor<-moduleTraitCor[order(moduleTraitCor[,grep("L1",colnames(moduleTraitCor))],decreasing=TRUE),]
 # } else if(orderBiotype=="Alu"){
-#  moduleTraitCor<-moduleTraitCor[order(moduleTraitCor[,grep("Alu",colnames(moduleTraitCor))],decreasing=TRUE),]
+# moduleTraitCor<-moduleTraitCor[order(moduleTraitCor[,grep("Alu",colnames(moduleTraitCor))],decreasing=TRUE),]
 # }
 
  moduleTraitPvalue.id<-match(rownames(moduleTraitCor),rownames(moduleTraitPvalue))
  moduleTraitPvalue<-moduleTraitPvalue[moduleTraitPvalue.id,]
- 
+ write.csv(moduleTraitPvalue,file="Correlation_Matrix_PValues_ModuleRepeat-CPM-Biotype-TxBiotype.csv")
+
  ###label the rows of the heatmap by numbers
 key<-data.frame(module=rownames(moduleTraitCor),id=seq(1:length(rownames(moduleTraitCor))),stringsAsFactors=FALSE)
+ write.csv(key,file="ModuleNameColor_key_ModuleNumber.csv")
 
  ###color code the rownames based on pathPairing
  ##add a legened
@@ -93,13 +96,23 @@ colnames(df_annot)[i+1]<-names(xN)[i]
   moduleTraitCor2<-moduleTraitCor
   key.id2<-match(rownames(moduleTraitCor2),key$module)
   rownames(moduleTraitCor2)<-key$id[key.id2]
+ write.csv(moduleTraitCor2,file="Correlation_Matrix_ModuleRepeat-CPM-Biotype-TxBiotype.csv")
+
   par(mar = c(6, 10, 3, 3));
  # Display the correlation values within a heatmap plot
   plot.new()
  if(nrow(moduleTraitCor2)>6){
   x.pv<-pvclust(moduleTraitCor2,nboot=100)
   
-  heatCor<-Heatmap(moduleTraitCor2,cluster_columns=x.pv$hclust,cluster_rows=FALSE,row_names_side="left",name="cor(x)", column_title = paste0("Module-Repeat ",how," Biotype relationships"))
+  heatCor<-Heatmap(moduleTraitCor2,cluster_columns=x.pv$hclust,cluster_rows=FALSE,row_names_side="left",name="cor(x)", column_title = paste0("Module-Repeat ",how," Biotype relationships (*<0.06)"),cell_fun=function(j,i,x,y,w,h,col){
+        weighted.Pvalue<-corPvalueStudent(moduleTraitCor2,ncol(moduleTraitCor2)-2)
+                   if(weighted.Pvalue[i,j]<0.06){
+                 #  grid.text(sprintf("%.3f", weighted.Pvalue[i,j]),x,y)
+                    grid.text("*",x,y-0.005 )
+                    }
+                   grid.rect(x,y,w,h,gp=gpar(fill=NA,col="black"))
+           }     
+         )
   print(modA+heatCor)
   } else{
  heatCor<-Heatmap(moduleTraitCor,cluster_rows=FALSE,row_names_side="left",name="cor(x)", column_title = paste0("Module-Repeat ",how," Biotype relationships"))
@@ -136,6 +149,10 @@ rownames(activation.direction)<-rownames(mC)
 colnames(activation.direction)<-names(xN)
 for(i in 1:length(pathwaysToPick)) {
  df<-pickPathway(qusageDbLite(qdbname),keyWord=pathwaysToPick[i])
+ df<-df[which(names(df)!="kexp")]
+  ##print to csv
+  activation.output<-ldply(df,data.frame)
+  write.csv(activation.output,file=paste0("Activation.Direction.Pathways.",pathwaysToPick[i],"_AcrossModules.csv"  ) )
   if(length(df)==0){
   next
  }
@@ -158,7 +175,16 @@ for(i in 1:length(pathwaysToPick)) {
   if(nrow(mC)>4){
   x.pv2<-pvclust(mC,nboot=200)
 
-  heatCor2<-Heatmap(mC,cluster_columns=x.pv2$hclust,cluster_rows=FALSE,row_names_side="left",name="cor(x)", column_title = paste0("Immune-Related ",how," Biotype relationships"))
+  heatCor2<-Heatmap(mC,cluster_columns=x.pv2$hclust,cluster_rows=FALSE,row_names_side="left",name="correlation(x)", column_title = paste0("Immune-Related ",how," (*<0.06)"), cell_fun=function(j,i,x,y,w,h,col){
+        weighted.Pvalue<-corPvalueStudent(mC,nrow(mC)-2)
+                   if(weighted.Pvalue[i,j]<0.06){
+                 #  grid.text(sprintf("%.3f", weighted.Pvalue[i,j]),x,y)
+                    grid.text("*",x,y-0.005 )
+                    }
+                   grid.rect(x,y,w,h,gp=gpar(fill=NA,col="black"))
+           }
+
+       )
   print(modB+heatCor2+activation.Heat)
   } else{
  heatCor<-Heatmap(moduleTraitCor,cluster_rows=FALSE,row_names_side="left",name="cor(x)", column_title = paste0("Immune-Related ",how," Biotype relationships"))
@@ -178,21 +204,34 @@ for(i in 1:length(pathwaysToPick)) {
   }else if(ncol(as.data.frame(moduleTraitCor))==1){
   rTraitCor<-rTraitCor[,colnames(rTraitCor)%in%names(moduleTraitCor)]
   }
+  write.csv(rTraitCor,file="Correlation-RepeatModules-RepeatBiotypes-Repeat_Module_Correlations.csv")
   corrMap<-bicor(t(moduleTraitCor),t(rTraitCor))
   corrMap.pvalue<-corPvalueStudent(corrMap,ncol(corrMap))
   corrMap.pvalue[which(is.na(corrMap.pvalue))]<-1
-
+  write.csv(corrMap,file="Correlation-GeneModules-RepeatModules-Pvalues.csv")
 
 
  ####
   corrMap2<-corrMap
   key.id2<-match(rownames(corrMap2),key$module)
   rownames(corrMap2)<-key$id[key.id2]
+    write.csv(corrMap2,file="Correlation-GeneModules-RepeatModules-Pvalues.csv")
+
   par(mar = c(6, 10, 3, 3));
  # Display the correlation values within a heatmap plot
   plot.new()
   cor.pv<-pvclust(corrMap2,nboot=100)
- map.heatCor<-Heatmap(corrMap2,cluster_columns=cor.pv$hclust,cluster_rows=FALSE,row_names_side="left",name="correlation(x)", column_title = paste0("Repeat Level ",how," Module relationships"))
+ map.heatCor<-Heatmap(corrMap2,cluster_columns=cor.pv$hclust,cluster_rows=FALSE,row_names_side="left",name="correlation(x)", column_title = paste0("Repeat Level ",how," Module (*<0.06)"),cell_fun=function(j,i,x,y,w,h,col){
+        weighted.Pvalue<-corPvalueStudent(corrMap2,25-2)
+                   if(weighted.Pvalue[i,j]<0.06){
+                 #  grid.text(sprintf("%.3f", weighted.Pvalue[i,j]),x,y)
+                    grid.text("*",x,y-0.005 )
+                    }
+                   grid.rect(x,y,w,h,gp=gpar(fill=NA,col="black"))
+           }
+
+
+  )
   print(modA+map.heatCor)
 
 ###########
@@ -204,10 +243,30 @@ for(i in 1:length(pathwaysToPick)) {
  if(nrow(corrMap.sub)>4){
   sub.pv<-pvclust(corrMap.sub,nboot=200)
 
-  sub.heatCor2<-Heatmap(corrMap.sub,cluster_columns=sub.pv$hclust,cluster_rows=FALSE,row_names_side="left",name="correlation(x)", column_title = paste0("Immune-Related ",how," Module relationships"))
+  sub.heatCor2<-Heatmap(corrMap.sub,cluster_columns=sub.pv$hclust,cluster_rows=FALSE,row_names_side="left",name="correlation(x)", column_title = paste0("Immune-Related ",how," Module relationships"), 
+cell_fun=function(j,i,x,y,w,h,col){
+        weighted.Pvalue<-corPvalueStudent(corrMap.sub,25-2)
+                   if(weighted.Pvalue[i,j]<0.06){
+                 #  grid.text(sprintf("%.3f", weighted.Pvalue[i,j]),x,y)
+                    grid.text("*",x,y-0.005 )
+                    }
+                   grid.rect(x,y,w,h,gp=gpar(fill=NA,col="black"))
+           }
+
+   )
   print(modB+sub.heatCor2+activation.Heat)
   } else{
- heatCor<-Heatmap(moduleTraitCor,cluster_rows=FALSE,row_names_side="left",name="cor(x)", column_title = paste0("Immune-Related ",how," Biotype relationships"))
+ heatCor<-Heatmap(moduleTraitCor,cluster_rows=FALSE,row_names_side="left",name="cor(x)", column_title = paste0("Immune-Related ",how," Biotype relationships"),
+ cell_fun=function(j,i,x,y,w,h,col){
+        weighted.Pvalue<-corPvalueStudent(moduleTraitCor,25-2)
+                   if(weighted.Pvalue[i,j]<0.06){
+                 #  grid.text(sprintf("%.3f", weighted.Pvalue[i,j]),x,y)
+                    grid.text("*",x,y-0.005 )
+                    }
+                   grid.rect(x,y,w,h,gp=gpar(fill=NA,col="black"))
+           }
+
+ )
   print(modB+sub.heatCor2+activation.Heat)
   }
   readkey()
