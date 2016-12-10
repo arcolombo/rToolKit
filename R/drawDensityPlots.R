57a21b98cf7d164d3712a140076c54dfbb98ea2b
root <anthonycolombo60@gmail.com>
2016-12-09 19:27:08 -0800

    updating drawDensity

--- R/drawDensityPlots.R
+++ R/drawDensityPlots.R
@@ -1,5 +1,15 @@
 #' @title Density plots and custom imaging
 #' @param kexp this is the non-stage level kexp for 3 stages 
+#' @param comparison the comparison group
+#' @param control the reference group
+#' @param comparison2 if more than one comparison
+#' @param numberComparisons  integer, the number of comparisons
+#' @param read.cutoff integer, to filter for repeatWise Analysis
+#' @param adjustBy either none,BH,BY, or holm for lmFit call
+#' @param wilcox.Alternative either less, which is testing the alternative that the comparison is less than the control, or greater which tests the alternative hypothesis that the comparison is greater than control.
+#' @param grob.comparison1 this is the text printed to plot that verbose the alternative
+#' @param grob.comparison2 this is the second printed explanation of the alternative hypothesis printed on plot testing medians
+#' @param testMedians  boolean , whether to include a median test of logFC from the repeat linear modeling.
 #' @import arkas
 #' @import ggplot2
 #' @import grid
@@ -42,19 +52,19 @@ dat1<-data.frame(logFC=c(abs(pHSC),LSC=abs(lsc)),Stage=lines1)
   pp<-(ggplot(dat,aes(x=logFC,fill=Stage ))+geom_density(alpha=0.5)+ggtitle(paste0("Differential Repeat Expression ",comparison2,"-",control," FDR: ",adjustBy))+scale_fill_manual(values=c("blue","red")))
   print(pp)
    }else if(testMedians==TRUE){
-   my_grob= grobTree(textGrob(paste0(grob.comparison2,wilcox.pvalue ),x=0.7,y=0.95,gp=gpar(col="black",fontsize=10,fontface="italic")))
+   my_grob= grobTree(textGrob(paste0(grob.comparison2,wilcox.pvalue2 ),x=0.7,y=0.95,gp=gpar(col="black",fontsize=10,fontface="italic")))
  pp<-(ggplot(dat,aes(x=logFC,fill=Stage ))+geom_density(alpha=0.5)+ggtitle(paste0("Differential Repeat Expression ",comparison2,"-",control," FDR: ",adjustBy))+scale_fill_manual(values=c("blue","red"))+annotation_custom(my_grob) )
    print(pp)
    }
   
   readkey()
 
-  pdf("Density_Stage_Plots.pdf")
+  pdf(paste0("Density_Stage_Plots_",comparison,".",control,".",comparison2,".pdf"))
   print(pp1)
   print(pp)
 dev.off()
  }else{
-  pdf("Density_Stage_Plots.pdf")
+  pdf(paste0("Density_Stage_Plots_",comparison,".",control,".pdf"))
   print(pp1)
   dev.off()
   }
